ee65952ab7c717d05a230288887829df2dc3ca2c
M. G.  Sherin <110461338+Sherin-MG@users.noreply.github.com>
2022-08-19 17:14:35 +0530

    Update EditBoxes.java

--- Selenium Tutorial/src/selenium/EditBoxes.java
+++ Selenium Tutorial/src/selenium/EditBoxes.java
@@ -16,7 +16,7 @@ public class EditBoxes {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("http://www.leafground.com/");
+		driver.get("http://testleaf.herokuapp.com/");
 		
 		driver.findElement(By .xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[1]/a/img")).click();
 		
